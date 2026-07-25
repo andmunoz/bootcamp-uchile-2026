826d534413fa7ebf036b9b054a3b7fc02d32f120
Andrés Muñoz O. <andmunoz@gmail.com>
2026-07-24 23:09:29 -0400

    People Counter changes in class

--- Ejemplos/PeopleCounter/app/src/main/java/cl/uchile/dcc/mobile/peoplecounter/ui/MainActivity.kt
+++ Ejemplos/PeopleCounter/app/src/main/java/cl/uchile/dcc/mobile/peoplecounter/ui/MainActivity.kt
@@ -7,17 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -41,60 +46,93 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PeopleCounter(modifier: Modifier = Modifier.Companion) {
+    var nombre by remember { mutableStateOf("") }
+    val personas: MutableList<String> = remember { mutableListOf() }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize()
+        verticalArrangement = Arrangement.Top,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp, 0.dp)
     ) {
         Text(
-            text = "People Counter App",
-            style = MaterialTheme.typography.titleLarge
-        )
-        Spacer(
-            modifier = Modifier.height(16.dp)
+            text = "PEOPLE COUNTER",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(16.dp)
         )
         RowCounter()
-        Spacer(
-            modifier = Modifier.height(16.dp)
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = { it ->
+                nombre = it
+            },
+            label = { Text("Nombre del Asistente") },
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
         )
         Button(
-            onClick = { /*TODO*/ }
+            onClick = {
+                personas.add(nombre)
+                nombre = ""
+            },
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
         ) {
             Text(
-                text = "Count!",
+                text = "Registrar",
                 modifier = Modifier.padding(24.dp)
             )
         }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            content = {
+                items(personas) { persona ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            text = persona,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                }
+            }
+        )
     }
 }
 
 @Composable
 fun RowCounter() {
-    Row() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
         Card() {
             Text(
                 text = "0",
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier.padding(32.dp, 16.dp),
                 style = MaterialTheme.typography.labelLarge
             )
         }
-        Spacer(
-            modifier = Modifier.width(8.dp)
-        )
         Card() {
             Text(
                 text = "0",
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier.padding(32.dp, 16.dp),
                 style = MaterialTheme.typography.labelLarge
             )
         }
-        Spacer(
-            modifier = Modifier.width(8.dp)
-        )
         Card() {
             Text(
                 text = "0",
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier.padding(32.dp, 16.dp),
                 style = MaterialTheme.typography.labelLarge
             )
         }
